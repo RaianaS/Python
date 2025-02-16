3ff5ad1159842cd6acb3e18b91393e9f686ee375
RaianaS <Raianacristina0905@outlook.com>
2025-02-16 20:10:31 +0000

    Pintando parede

---
+++ 11 Pintando Parede.py
@@ -0,0 +1,7 @@
+# Faça um programa que leia a largula e a altura de uma parede em metros. 
+#Calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m².
+
+larg = float(input('Largura da parede: '))
+alt = float(input('Altura da parede: '))
+área = larg * alt
+print('Sua parede tem a dimensão de {}x{} e sua área é de {}m².'.format(larg, alt, área)) 
