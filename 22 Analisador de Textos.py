91566fd455d513d0c5a03faf45053eac2ab9590b
RaianaS <Raianacristina0905@outlook.com>
2025-03-19 19:55:01 +0000

    Analisador de textos

---
+++ 22 Analisador de Textos.py
@@ -0,0 +1,8 @@
+#Crie um programa que leia o nome completo de uma pessoa e mostre:
+#O nome com todas as letras maiúsculas e minúsculas.
+
+nome = str(input('Digite seu nome completo: '))
+print('Analisando seu nome..')
+print('Seu nome em maiúsculasé {}'.format(nome.upper()))
+print('Seu nome em minúsculas é {}'.format(nome.lower()))
+
